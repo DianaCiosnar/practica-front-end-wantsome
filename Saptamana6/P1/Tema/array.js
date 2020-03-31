c8fd378464915d0bfb726a9548edb17dc4898f8d
DianaCiosnar <ciosnar.diana@gmail.com>
2020-03-31 11:11:03 +0300

    updated homework S6-P2-array

--- Saptamana6/P1/Tema/array.js
+++ Saptamana6/P1/Tema/array.js
@@ -113,3 +113,63 @@ function student(firstName, secondName, gender) {
 }
 var myStudent = new student("Diana", "Ciosnar", "famale");
 console.log(myStudent);
+
+//Ex1: Creati un array care sa aiba length-ul 5. Demonstrati ca length-ul este 5.
+var array = ['stundent1','stundent2','stundent3','student4','student5','student6'];
+var size=array.length;
+console.log(size);
+
+//Ex2: Din cele 2 arrayuri de mai jos, folositi o metoda sa avem unul singur
+var array1 = ['Car', 'Vehicle'];
+var array2 = ['House', 'Job'];
+var result=array1.concat(array2);
+console.log(result);
+
+//Ex3: Creati 2 variabile, una care sa dea true si una care sa dea false atunci cand folosim metoda isArray()
+var car1 = ['Car', 'Vehicle'];
+var car2 = 'Toyota';
+var result= Array.isArray(['Car', 'Vehicle']);
+console.log(result)
+var result1= Array.isArray('Toyota');
+console.log(result1)
+
+//Ex4: Folositi o metoda pe arrayuri avand in vedere variabila de mai jos, sa obtinem rezultatul "one and two and three and four and five" 
+// fara sa modificati variabila initiala
+var numbers = ['one', 'two', 'three', 'four', 'five'];
+var result = numbers.join(' and ');
+console.log(result);
+
+//Ex5: Adaugati 2 elemente noi in arrayul de mai jos
+var cars = ['Tesla', 'Dacia'];
+var result=cars.push('Toyota','Audi');
+console.log(result);
+
+//Ex6: Avand rezultatul de la exercitiul anterior, scoateti ultimul element intrat.
+cars.pop();
+console.log(cars);
+
+//Ex7: Avand rezultatul exercitiului anterior, adaugati elementul 'Dodge' la inceputul arrayului
+cars.unshift("Renault");
+console.log(cars);
+
+//Ex8: Avand rezultatul exercitiului anterior, inversati ordinea elementelor din array
+cars.reverse();
+console.log(cars);
+
+//Ex9: Avand in vedere rezultatul anterior Folositi o metoda pe arrayuri care sa inlocuiasca incepand de la elementul 'Tesla' si sa adauge alte 2 elemente noi - splice();
+cars.splice(2,0,"Audi","Ford");
+console.log(cars);
+
+//Ex10: Sortati urmatorul array de la mic la mare si de la mare la mic 
+var arrayNumbers = [1, 5, 20, 30, 100];
+arrayNumbers.sort(function (a, b) {
+    return a - b;
+});
+console.log(arrayNumbers);
+
+//Ex11 : Aveti urmatorul array - folositi o functie care sa ia arrayul ca argument si sa-l returneze ordonat
+var fruitsArray = ["Apple", "Lemon", "Banana", "Orange", "Bere", "Mici"]
+function sortArray(fruitsArray) {
+    sortArray.sort();
+}
+console.log(sortArray["Apple", "Lemon", "Banana", "Orange", "Bere", "Mici"]);
